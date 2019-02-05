261ba04cd6ea2843f69ff94bd39df059351586b5
soportecreatic <37717534+soportecreatic@users.noreply.github.com>
2019-02-05 16:31:07 -0500

    Add files via upload

---
+++ local_uservalform.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package   local_uservalform
+ * @copyright 2018, Creatic SAS <soporte@creatic.co>. Developed by Juan Ricardo Arenas.
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Data Validation Form';
+$string['uservalform'] = 'Data Validation';
+$string['navigationmenuname'] = 'Data Validation Form';
+$string['modalsettingstitle'] = 'Validation Form Options';
+$string['modalsettingscategory'] = 'Category: ';
+$string['enablevalidation'] = 'Enable data validation form';
+$string['enablevalidationdescription'] = 'Enables a form which allows the user to enter a specific course using a validation data. If the data matchs with the profile user data, it allows him/her to enter.';
+$string['showpopuponlyonce'] = 'Show form only once';
+$string['showpopuponlyoncedescription'] = 'If this option is enabled, form only appears when the user enters to the course for first time. Otherwise, it appears each time user logs in and enters the course';
+$string['addfieldbutton'] = 'Add field';
+$string['validationformtext'] = 'Before entering the course, We need to verify the student\'s information. Please, enter the correct information.';
+$string['send'] = 'Send';
+$string['dataerrorblank'] = 'Do not leave this field in blank.';
+$string['dataerror'] = 'The entered information is not correct.';
+$string['changessaved'] = 'Changes saved.';
+$string['developedby'] = 'Developed by Creatic SAS';
