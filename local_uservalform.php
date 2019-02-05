2e0ef28a5fd6763663d0bd6b83d584bf436ed191
soportecreatic <37717534+soportecreatic@users.noreply.github.com>
2019-02-05 16:32:09 -0500

    Add files via upload

---
+++ local_uservalform.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package   local_uservalform
+ * @copyright 2018, Creatic SAS <soporte@creatic.co>. Developed by Juan Ricardo Arenas.
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Formulario de validación de datos';
+$string['uservalform'] = 'Validación de datos';
+$string['navigationmenuname'] = 'Formulario de validación de datos';
+$string['modalsettingstitle'] = 'Opciones de formulario de validación';
+$string['modalsettingscategory'] = 'Categoría: ';
+$string['enablevalidation'] = 'Habilitar formulario de validación de datos';
+$string['enablevalidationdescription'] = 'Habilita un formulario el cual permite al usuario ingresar al curso usando un dato de validación. Si este dato coincide con los datos ingresados en su perfil, se le permitirá ingresar al curso.';
+$string['showpopuponlyonce'] = 'Mostrar ventana sólo una vez';
+$string['showpopuponlyoncedescription'] = 'Si se habilita esta opción el formulario sólo aparecerá cuando el usuario ingrese por primera vez al curso. En caso contrario aparecerá cada vez que el usuario inicia sesión e ingresa al curso.';
+$string['addfieldbutton'] = 'Agregar campo';
+$string['validationformtext'] = 'Antes de ingresar al curso, necesitamos verificar la información del estudiante. Por favor ingrese la información correcta.';
+$string['send'] = 'Enviar';
+$string['dataerrorblank'] = 'No se especificó ningún valor.';
+$string['dataerror'] = 'La información ingresada no es correcta.';
+$string['changessaved'] = 'Cambios guardados.';
+$string['developedby'] = 'Desarrollado por Creatic SAS';
